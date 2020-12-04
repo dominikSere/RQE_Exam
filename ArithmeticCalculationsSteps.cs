5e15cababd72cc5053dac1cef72a4e671b0600ca
dominikSere <74732912+dominikSere@users.noreply.github.com>
2020-12-04 14:31:54 +0100

    Add files via upload

---
+++ ArithmeticCalculationsSteps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using TechTalk.SpecFlow;
+
+namespace ExamRQE
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+        
+    {
+        private int number;
+        private int number2;
+        private int result;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            number = (int)p0;
+        }
+        
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p1)
+        {
+            number2 = (int)p1;
+        }
+        
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            result = number + number2;
+        }
+        
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            result = number - number2;
+        }
+        
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            result = number * number2;
+        }
+        
+        [When(@"the two numbers are divided")]
+        public void WhenTheTwoNumbersAreDivided()
+        {
+            result = number / number2;
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SpecFlowFeature1.feature
@@ -0,0 +1,35 @@
+﻿Feature: SpecFlowFeature1
+	As a User of a Calculator
+	I want to be able to add, subtract, multiply and divide numbers
+	So that I can calculate differrent arithmetic Calculations
+
+
+
+@mytag
+Scenario: Add two numbers
+	Given the first number is 6
+	And the second number is 3
+	When the two numbers are added
+	Then the result should be 9
+
+
+@mytag
+Scenario: Subtract two numbers
+	Given the first number is 6
+	And the second number is 3
+	When the two numbers are subtracted
+	Then the result should be 3
+
+	@mytag
+Scenario: Multiply two numbers
+	Given the first number is 6
+	And the second number is 3
+	When the two numbers are multiplied
+	Then the result should be 18
+
+	@mytag
+	Scenario: Divide two numbers
+Given the first number is 6
+And the second number is 3
+When the two numbers are divided
+Then the result should be 2
